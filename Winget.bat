325d207e2141b73918a72d9dc276170b97dbdae5
Ryan-L-UK <121292615+Ryan-L-UK@users.noreply.github.com>
2025-10-17 13:20:22 +0100

    Create Winget.bat

---
+++ Winget.bat
@@ -0,0 +1,24 @@
+:: INSTALL BELOW
+winget install --id Google.Chrome -h --accept-source-agreements --accept-package-agreements
+winget install --id Google.ChromeRemoteDesktopHost -h --accept-source-agreements --accept-package-agreements
+winget install --id PrivateInternetAccess.PrivateInternetAccess -h --accept-source-agreements --accept-package-agreements
+winget install --id TeamProwlarr.Prowlarr -h --accept-source-agreements --accept-package-agreements
+winget install --id Tweaking4All.RenameMyTVSeries -h --accept-source-agreements --accept-package-agreements
+winget install --id XPFFTQ032PTPHF -h --accept-source-agreements --accept-package-agreements
+winget install --id XPDM1ZW6815MQM -h --accept-source-agreements --accept-package-agreements
+
+:: UNINSTALL BELOW
+winget uninstall --id Microsoft.BingNews -h
+winget uninstall --id Microsoft.Teams -h
+winget uninstall --id 9P1J8S7CCWWT -h
+winget uninstall --id 9NZBF4GT040C -h
+winget uninstall --id 9WZDNCRFHVFW -h
+winget uninstall --id 9P7BP5VNWKX5 -h
+winget uninstall --id 9NFTCH6J7FHV -h
+winget uninstall --id 9WZDNCRFJ3Q2 -h
+winget uninstall --id 9NBLGGH4QGHW -h
+winget uninstall --id 9NRX63209R7B -h
+winget uninstall --id 9NBLGGH5R558 -h
+winget uninstall --id 9WZDNCRFJBBG -h
+winget uninstall --id 9NBLGGH4R32N -h
+winget uninstall --id 9MV0B5HZVK9Z -h
